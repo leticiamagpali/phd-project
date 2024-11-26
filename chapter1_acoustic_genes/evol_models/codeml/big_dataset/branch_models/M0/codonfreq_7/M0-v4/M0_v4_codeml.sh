5af6d9fc338e203fee97c4f82130be4bf50c50ee
Leticia Magpali <leticiamagpali@dal.ca>
2024-11-26 15:38:08 -0400

    Add new files for chapters and scripts

--- chapter1_acoustic_genes/evol_models/codeml/big_dataset/branch_models/M0/codonfreq_7/M0-v4/M0_v4_codeml.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-#$ -S /bin/bash 
-. /etc/profile 
-#$ -o /home/leticiamagpali/evol_models/codeml/branch_models/M0/M0_v4
-#$ -M leticiamagpali@dal.ca
-#$ -pe threaded 10
-cd /home/leticiamagpali/evol_models/codeml/branch_models/M0/M0_v4
-source activate paml
-codeml M0-v4.ctl
--- chapter1_acoustic_genes/evol_models/codeml/small_dataset/results/M0/M0_codeml.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-#$ -S /bin/bash 
-. /etc/profile 
-#$ -o /home/leticiamagpali/phd/evol_models/codeml/small_dataset/runs_models_test 
-#$ -M leticiamagpali@dal.ca
-#$ -m be 
-cd /home/leticiamagpali/phd/scripts 
-source activate paml
-python RunCodeml.py
